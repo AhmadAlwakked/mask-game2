a4b24f7c07994066abc21d875af6a16165e27942
pooh23896 <noah.rekers@gmail.com>
2026-01-28 12:19:26 +0100

    Game Scene player

--- mask game2/Assets/Scripts/MaskScalerUI.cs
+++ mask game2/Assets/Scripts/MaskScalerUI.cs
@@ -18,10 +18,10 @@ public class MaskScalerUI : MonoBehaviour
     public Vector2 enlargedSize = new Vector2(130, 130);
 
     [Header("Panel Settings")]
-    public Image panel;
-    public Color mask1Color = new Color(1f, 0f, 0f, 0.5f);
-    public Color mask2Color = new Color(0f, 1f, 0f, 0.5f);
-    public Color mask3Color = new Color(0f, 0f, 1f, 0.5f);
+    public Image panel; // Panel dat van kleur verandert
+    public Color mask1Color = new Color(1f, 0f, 0f, 0.5f); // Rood, 50% transparant
+    public Color mask2Color = new Color(0f, 1f, 0f, 0.5f); // Groen, 50% transparant
+    public Color mask3Color = new Color(0f, 0f, 1f, 0.5f); // Blauw, 50% transparant
 
     [Header("Mask States")]
     public bool isMask1Active = false;
@@ -36,38 +36,34 @@ public class MaskScalerUI : MonoBehaviour
 
     void Update()
     {
+        // Keys om masks te switchen
         if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
             EquipMask(mask1, mask1Color, mask1Object);
-        }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
             EquipMask(mask2, mask2Color, mask2Object);
-        }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
             EquipMask(mask3, mask3Color, mask3Object);
-        }
     }
 
+    // Equip een masker: vergroot, verander panelkleur, activeer masker object en update bool
     void EquipMask(Image mask, Color panelColor, GameObject maskObject)
     {
-        ResetMasks(); // reset alle maskers en bools
+        ResetMasks(); // reset alle masks en bools
         SetMaskSize(mask, enlargedSize);
         SetPanelColor(panelColor);
         ActivateMaskObject(maskObject);
-        SetMaskBool(maskObject, true); // zet de bijbehorende bool op true
+        SetMaskBool(maskObject, true);
     }
 
+    // Zet alle masks terug naar normale grootte en zet masker objects uit
     void ResetMasks()
     {
         SetMaskSize(mask1, normalSize);
         SetMaskSize(mask2, normalSize);
         SetMaskSize(mask3, normalSize);
 
-        // Zet alle masker objects uit
         if (mask1Object != null) mask1Object.SetActive(false);
         if (mask2Object != null) mask2Object.SetActive(false);
         if (mask3Object != null) mask3Object.SetActive(false);
@@ -81,34 +77,25 @@ public class MaskScalerUI : MonoBehaviour
     void SetMaskSize(Image mask, Vector2 size)
     {
         if (mask != null)
-        {
             mask.rectTransform.sizeDelta = size;
-        }
     }
 
     void SetPanelColor(Color color)
     {
         if (panel != null)
-        {
             panel.color = color;
-        }
     }
 
     void ActivateMaskObject(GameObject maskObject)
     {
         if (maskObject != null)
-        {
             maskObject.SetActive(true);
-        }
     }
 
     void SetMaskBool(GameObject maskObject, bool state)
     {
-        if (maskObject == mask1Object)
-            isMask1Active = state;
-        else if (maskObject == mask2Object)
-            isMask2Active = state;
-        else if (maskObject == mask3Object)
-            isMask3Active = state;
+        if (maskObject == mask1Object) isMask1Active = state;
+        else if (maskObject == mask2Object) isMask2Active = state;
+        else if (maskObject == mask3Object) isMask3Active = state;
     }
 }
